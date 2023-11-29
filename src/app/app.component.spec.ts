43bfcf564b76890d8988c83d9b671e59205e55a6
daryelis95 <daryelis95@gmail.com>
2023-11-29 17:29:58 -0300

    mejoras de vista

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tesisp-app' title`, () => {
+  it(`should have the 'appTesis' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tesisp-app');
+    expect(app.title).toEqual('appTesis');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tesisp-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, appTesis');
   });
 });
